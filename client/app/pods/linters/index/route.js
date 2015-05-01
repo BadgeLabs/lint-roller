f59162194b7d9ce48696b457dd8004b5086705c4
Ian VanSchooten <ian@badgelabsllc.com>
2015-04-30 21:29:41 -0400

    Display list of linters

--- client/app/pods/linters/index/route.js
+++ client/app/pods/linters/index/route.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function findAllLinters(){
+    return this.store.findAll('linter');
+  },
   actions: {
     gotoNewLinter: function gotoNewLinter(){
       this.transitionTo('linters.new');
